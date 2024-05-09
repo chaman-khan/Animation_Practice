94d928e8483f879d2b3baead3e4a47c96f9e7c11
chaman-khan <chaman.devv@gmail.com>
2024-05-09 21:54:09 +0500

    withSpring_and_useAnimatedStyle

--- src/screens/onboarding.js
+++ src/screens/onboarding.js
@@ -1,47 +1,52 @@
-import React, {useEffect, useRef} from 'react';
-import {Animated, Text, View} from 'react-native';
-
-const FadeInView = props => {
-  const fadeAnim = useRef(new Animated.Value(0)).current;
-  useEffect(() => {
-    Animated.timing(fadeAnim, {
-      toValue: 5,
-      duration: 20000,
-      useNativeDriver: true,
-    }).start();
-  }, [fadeAnim]);
-
-  return (
-    <Animated.View
-      style={{
-        ...props.style,
-        opacity: fadeAnim, // Bind opacity to animated value
-      }}>
-      {props.children}
-    </Animated.View>
-  );
-};
+import React from 'react';
+import {Button, StyleSheet, View} from 'react-native';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from 'react-native-reanimated';
 
 const OnBoarding = () => {
+  const width = useSharedValue(100);
+  const translateX = useSharedValue(0);
+  const handlePress = () => {
+    width.value = withSpring(width.value + 50);
+    translateX.value += 50;
+  };
+  const animatedStyles = useAnimatedStyle(() => ({
+    transform: [{translateX: withSpring(translateX.value * 2)}],
+  }));
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}>
-      <FadeInView
-        style={{
-          width: 250,
-          height: 50,
-          backgroundColor: 'powderblue',
-        }}>
-        <Text style={{fontSize: 28, textAlign: 'center', margin: 10}}>
-          Fading in
-        </Text>
-      </FadeInView>
+    <View style={styles.container}>
+      <Animated.View
+        style={Object.assign(Object.assign({}, styles.box), {width})}
+      />
+      <Animated.View style={[styles.box2, animatedStyles]} />
+      <View style={{alignSelf: 'center'}}>
+        <Button onPress={handlePress} title="Click Me" />
+      </View>
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  box: {
+    alignSelf: 'center',
+    height: 100,
+    backgroundColor: '#b58df1',
+    borderRadius: 20,
+    marginVertical: 64,
+  },
+  box2: {
+    alignSelf: 'flex-start',
+    width: 100,
+    height: 100,
+    backgroundColor: '#b58df1',
+    borderRadius: 20,
+    marginVertical: 64,
+  },
+});
 export default OnBoarding;
